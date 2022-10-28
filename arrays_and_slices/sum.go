da515e344ce9076511a49e5f2b09bcea69795a6e
shaf <shafeelinks@gmail.com>
2022-10-28 14:24:01 -0400

    Add sum tests and logic

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -2,9 +2,22 @@ package arrays_and_slices
 
 func Sum(numbers []int) int {
 	sum := 0
-	for _, val := range numbers {
-		sum += val
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
-	return sum
+	return sums
 }
