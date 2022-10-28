65e9a0808838e46f1b86cc5f87550f31564f2fa4
shaf <shafeelinks@gmail.com>
2022-10-28 11:27:25 -0400

    Refactor assertion into a helper function

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,31 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Shafee")
-	want := "Hello, Shafee!"
+
+	t.Run("Saying hello to user", func(t *testing.T) {
+		got := Hello("Shafee")
+		want := "Hello, Shafee"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("Say 'Hello, World' when an argument is empty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// tell the test suite that this method is a helper
+	t.Helper()
 
 	if got != want {
-		t.Errorf("Got %q. Wanted %q.", got, want)
+		t.Errorf("got %q, want %q", got, want)
 	}
 }
